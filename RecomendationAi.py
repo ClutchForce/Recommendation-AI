09421d699a02738bdb26d9756a65d12241609069
Paul Gherghel <pagherghel@yahoo.com>
2023-12-07 15:56:00 -0500

    not found fix

--- RecomendationAi.py
+++ RecomendationAi.py
@@ -1,5 +1,7 @@
 
+#Pandas library used for data handling.
 import pandas as pd
+#Computes the cosine similarity between sub-categories.
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_all_customer_names(file_path):
@@ -16,6 +18,7 @@ def print_customer_names_in_columns(names, columns=5):
             print()
     print()  # Print a final new line at the end
 
+#Data loading an processing
 def load_and_preprocess_data(file_path):
     # Load the data
     data = pd.read_csv(file_path)
@@ -27,10 +30,12 @@ def load_and_preprocess_data(file_path):
     data['Customer Name'] = data['Customer Name'].str.lower()  
     
     # Pivot to create the user-item interaction matrix for sub-categories
+    # essential for personalized recommendations.
     user_item_matrix = pd.pivot_table(data, index='Customer Name', columns='Sub Category', aggfunc='size', fill_value=0)
     
     return user_item_matrix
 
+#Calculating Item Similarity
 def calculate_item_similarity(user_item_matrix):
     # Calculate the cosine similarity between sub-categories
     item_similarity = cosine_similarity(user_item_matrix.T)
@@ -38,14 +43,18 @@ def calculate_item_similarity(user_item_matrix):
     
     return item_similarity_df
 
+#Personalized Recommendation
 def recommend_for_user_modified(user_name, user_item_matrix, item_similarity_matrix, top_n=5):
     # Modified recommendation function
+
+    # Check if the user exists in the dataset
     if user_name not in user_item_matrix.index:
         return f"User '{user_name}' not found in the dataset."
 
     user_purchases = user_item_matrix.loc[user_name]
     recommendations = {}
 
+    # Iterate over all sub-categories purchased by the user
     for sub_category, quantity in user_purchases.items():
         similar_items = item_similarity_matrix[sub_category]
         for similar_item, similarity in similar_items.items():  # Changed from iteritems() to items()
@@ -53,6 +62,7 @@ def recommend_for_user_modified(user_name, user_item_matrix, item_similarity_mat
                 weighted_similarity = similarity * (1 + quantity)
                 recommendations[similar_item] = recommendations.get(similar_item, 0) + weighted_similarity
 
+    # Sort the recommendations by their weighted similarity for the top n recommendations
     recommended_sub_categories = sorted(recommendations, key=recommendations.get, reverse=True)[:top_n]
     return recommended_sub_categories
 
@@ -71,6 +81,11 @@ try:
         if user_name.lower() == 'exit':  # Convert input to lower case for case-insensitive comparison
             break
         recommended_sub_categories = recommend_for_user_modified(user_name.lower(), user_item_matrix, item_similarity_df)
-        print(f"Recommended sub-categories for '{user_name}': {recommended_sub_categories}")
+        print(f"Recommended sub-categories for '{user_name}':")
+        if (recommended_sub_categories == f"User '{user_name}' not found in the dataset."):
+            print(recommended_sub_categories)
+        else:
+            for i, category in enumerate(recommended_sub_categories, 1):
+                print(f"{i}. {category}")
 except KeyboardInterrupt:
     print("\nScript terminated by user.")
